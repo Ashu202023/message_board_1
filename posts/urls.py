70f1a29245ba9c97d6ede569155b431817adc1b0
Ashu202023 <ashutosh20kumar23@gmail.com>
2025-03-27 09:42:20 +0530

    add comment in url.py file for better understanding

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from .views import PostL
-
+# in case of class based view ,we have to call as_view() method
 urlpatterns=[
     path("",PostL.as_view(),name="post_l"),
 ]
